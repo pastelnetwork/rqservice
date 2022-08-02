fd4f1f2d9e2aaa0171aa3d04b04f380ec6474317
a-ok123 <a-ok123@gmail.com>
2022-08-01 23:47:42 -0400

    version bumped

--- src/app.rs
+++ src/app.rs
@@ -62,7 +62,7 @@ impl ServiceSettings {
 
     fn cmd_args_new(config_path: &str) -> ArgMatches<'static> {
         App::new("rqservice")
-            .version("v1.1.0")
+            .version("v1.1.1")
             .author("Pastel Network <pastel.network>")
             .about("RaptorQ Service")
             .arg(Arg::with_name("config")
