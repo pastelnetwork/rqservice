32244c5beb2ed003d19b8d7ad7e1c1d4fbd93f12
Jeff Emanuel <35050222+Dicklesworthstone@users.noreply.github.com>
2023-08-15 22:32:26 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,18 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 use flexi_logger::{Logger, FileSpec, WriteMode};
+use std::sync::Arc;
 
 pub mod app;
 pub mod rqserver;
 pub mod rqprocessor;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
+use std::str::FromStr;
+use cron::Schedule;
+use std::thread;
+use chrono::{Utc, Duration as ChronoDuration};
+use rqprocessor::RaptorQProcessor;
 
 // pub const DB_PATH: &str = "/home/ubuntu/.pastel/testnet3/rq_symbols.sqlite";
 pub const DB_PATH: &str = "/home/ubuntu/rqservice/test_files/rq_symbols.sqlite";
@@ -31,7 +37,36 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     rqprocessor::initialize_database(DB_PATH).unwrap();
 
     let manager = SqliteConnectionManager::file(rqprocessor::DB_PATH);
-    let pool = Pool::new(manager).expect("Failed to create pool.");
+    let pool = Arc::new(Pool::new(manager).expect("Failed to create pool."));
+
+    // Create the RaptorQProcessor instance
+    let rq_processor = RaptorQProcessor::new(DB_PATH)?;
+
+    // Clone the Arc for the spawned thread
+    let pool_clone = Arc::clone(&pool);
+
+    // Spawn a background thread to run the maintenance task
+    thread::spawn(move || {
+        let cron_expression = "0 3 * * *"; // Run daily at 3:00 AM
+        let schedule = Schedule::from_str(cron_expression).unwrap();
+
+        // Iterate over the schedule to execute the task
+        for datetime in schedule.upcoming(Utc) {
+            let now = Utc::now();
+            let time_until_next_job = datetime.signed_duration_since(now);
+            if time_until_next_job > ChronoDuration::seconds(0) {
+                thread::sleep(time_until_next_job.to_std().unwrap());
+            }
+
+            // Get a connection from the pool
+            if let Ok(conn) = pool_clone.get() {
+                // Execute the maintenance function
+                if let Err(err) = rq_processor.db_maintenance_func(&conn) {
+                    log::error!("Error during database maintenance: {:?}", err);
+                }
+            }
+        }
+    });
 
     rqserver::start_server(&settings, &pool).await?;
 
